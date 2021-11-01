12ac8646114065615e14d93338943b16ad368019
Smuzzy_waiii <smaran.jawalkar@gmail.com>
2021-11-01 09:50:06 +0530

    cleaning updates

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -6,25 +6,22 @@ String? auth_register(String username, String password) {
   String? response = null;
 }
 
-Future<Map?> auth_signin(String username, String password) async
-{
+Future<Map?> auth_signin(String username, String password) async {
   Map? response = null;
   var event = await FirebaseFirestore.instance.doc('Users/User').get();
-    Map<String, dynamic> users = event.data() as Map<String, dynamic>;
-    print(users);
-    if (users[username] == null){
+  Map<String, dynamic> users = event.data() as Map<String, dynamic>;
+  print(users);
+  if (users[username] == null) {
+    response = null;
+  } else {
+    var user = users[username] as Map;
+    print(user);
+    if (user['password'].toString() != password) {
       response = null;
+    } else {
+      user['username'] = username;
+      response = user;
     }
-    else {
-      var user = users[username] as Map;
-      print(user);
-      if (user['password'].toString()!=password){
-        response = null;
-      }
-      else {
-        user['username'] = username;
-        response = user;
-      }
-    }
+  }
   return response;
 }
